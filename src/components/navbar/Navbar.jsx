c487053989f57ede206c3256f9679ffb11f6eb64
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-23 14:15:58 +0530

    add animations for name tag

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './navbar.scss'
+import {motion} from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       {/*sidebar */}
       <div className="wrapper">
-        <span>Himasha Nethmini</span>
+        <motion.span initial={{opacity:0, scale:0.5}} 
+        animate={{opacity:1, scale:1}}
+        transition={{duration:0.5}}>
+          Himasha Nethmini
+        </motion.span>
 
         <div className="social">
 
