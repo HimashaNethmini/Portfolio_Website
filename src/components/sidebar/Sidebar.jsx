7921edec2a22e7de53e6bf74f227e07667dd6380
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-23 21:46:53 +0530

    add use state

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './sidebar.scss'
+import ToggleButton from './toggleButton/ToggleButton'
+import Links from "./links/Links"
+import { motion } from "framer-motion"
+
 
 const Sidebar = () => {
+  const [ open, setOpen ] = useState(false)
+
+  const variants = {
+    open:
+    closed:
+
+  }
+
+
   return (
-    <div>
+    <motion.div className='sidebar' animate={open ? "open" : "closed"}>
       
-    </div>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+
+      </motion.div>
+
+      <ToggleButton setOpen = {setOpen} />
+    </motion.div>
   )
 }
 
