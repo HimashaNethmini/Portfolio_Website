e5f9a0b1a1782c07159dbd0d0538028c9fbd6ad6
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-24 19:28:03 +0530

    add component navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import "./app.scss"
+import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
-  return <div>
+  return (
+    <div>
+      {/* linking to the website */}
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
 
-{/* linking to the website */}
-    <section id = "Homepage"> 
-      <Navbar /> 
-      <Hero />
-    </section>
-    
-    <section id="Services"> Parallax </section>
-    <section > Services </section>
-    <section id = "Portfolio"> Parallax </section>
-    <section> Portfolio1 </section>
-    <section> Portfolio2 </section>
-    <section> Portfolio3 </section>
-    <section id="Contact"> 
-      Contact 
-    </section>
-  </div>;
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        {/*<Services />*/}
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      {/*<Portfolio />*/}
+      <section id="Contact">
+        {/*<Contact />*/}
+      </section>
+     
+    </div>
+  );
 };
 
 export default App;
