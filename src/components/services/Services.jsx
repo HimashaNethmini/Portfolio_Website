f3e1e2731585077f3c8cfc7325bb3a92db628539
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-24 21:15:39 +0530

    add text part

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import './services.scss'
+import {motion} from "framer-motion"
 
 const Services = () => {
   return (
-    <div className='services'>
+    <motion.div className='services'>
+
+        <motion.div className="textContainer">
+            <p>I am passionate about learning and growing
+                <br />
+             in a wide range of technologies </p>
+            <hr />
+        </motion.div>
+
+        <motion.div className="titleContainer">
+            <div className="title">
+                <img src="/people.webp" alt="" />
+                <h1><b>Unique</b> Ideas</h1>
+            </div>
+
+            <div className="title">
+                <h1>
+                    <b>FOR YOUR</b>Business
+                </h1>
+                <button>What we do</button>
+            </div>
+        </motion.div>
+        
+        <motion.div className="list"></motion.div>
       
-    </div>
+    </motion.div>
   )
 }
 
