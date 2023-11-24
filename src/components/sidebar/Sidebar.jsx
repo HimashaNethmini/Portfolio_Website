03e5b4cce6d09bab7a7fc4757a1bb2e5c486f6af
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-24 10:27:35 +0530

    completed the sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,21 +4,38 @@ import ToggleButton from './toggleButton/ToggleButton'
 import Links from "./links/Links"
 import { motion } from "framer-motion"
 
-
-const Sidebar = () => {
-  const [ open, setOpen ] = useState(false)
-
+//animation 
   const variants = {
-    open:
-    closed:
+    open: {
+      clipPath: "circle (1200px at 50x 50px)",
+      transition: {
+        type: "spring",
+        stiffness:20,
+      },
 
-  }
+    },
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type:"spring",
+        stiffness: 400,
+        damping: 40,
+      },
 
+    },
+
+  };
+
+  const Sidebar = () => {
+    const [ open, setOpen ] = useState(false)
 
   return (
     <motion.div className='sidebar' animate={open ? "open" : "closed"}>
       
       <motion.div className="bg" variants={variants}>
+
+        {/*rendered link component */}
         <Links />
 
       </motion.div>
