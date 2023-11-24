abb05216e01fa0ced54d84b519cd72b70c94b83a
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-24 12:20:03 +0530

    add animations

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -32,10 +32,10 @@ const Links = () => {
       {items.map((item) => (
         <motion.a
           href={`#${item}`}
-          key={item}
+          key={item} 
           variants={itemVariants}
           whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
+          whileTap={{ scale: 0.95 }} 
         >
           {item}
         </motion.a>
