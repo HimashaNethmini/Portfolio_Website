715bf4f65733c52ffb304c89bf79c52370e14b98
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-24 21:05:46 +0530

    rendered in main

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return (
@@ -15,9 +16,11 @@ const App = () => {
       <section id="Services">
         <Parallax type="services" />
       </section>
+
       <section>
-        {/*<Services />*/}
+        <Services />
       </section>
+      
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
