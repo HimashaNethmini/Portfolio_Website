28001a0c3915745cbd0ad85cbed5d70ad3fb9266
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-24 18:21:28 +0530

    completed hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,25 +1,25 @@
-import React from "react";
 import "./hero.scss";
 
 const Hero = () => {
   return (
     <div className="hero">
-      {/* title */}
-      <div className="textContainer">
-        <h2>I'M A </h2>
-        <h1>Web developer and UI designer</h1>
-
-        {/*buttons */}
-        <button>See the Latest Work</button>
-        <button>Contact Me</button>
-
-        {/*small image */}
-        <img src="/scroll.png" alt="" />
+      <div className="wrapper">
+        <div
+          className="textContainer">
+          <h2>IM A</h2>
+          <h1>
+            Web developer and UI designer
+          </h1>
+          <div className="buttons">
+            <button>
+              See the Latest Works
+            </button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="" />
+        </div>
       </div>
-
       
-
-      {/* main image */}
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
       </div>
