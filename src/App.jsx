b2d94f38b813a0245512eaaaa7252490523f5d17
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-26 21:11:55 +0530

    import portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
   return (
@@ -24,7 +25,8 @@ const App = () => {
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
-      {/*<Portfolio />*/}
+        <Portfolio />
+
       <section id="Contact">
         {/*<Contact />*/}
       </section>
