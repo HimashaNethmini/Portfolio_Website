60950549b80370a9da724d479dff1fe28e141543
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-27 10:19:48 +0530

    Make the default form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './contact.scss';
+import React from "react";
+import "./contact.scss";
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="contact">
+      <div className="textContainer">
+        <h1>Let's work together</h1>
+      </div>
 
-export default Contact
+      <div className="formContainer">
+        <form>
+          <input type="text" placeholder="Name" required/>
+          <input type="email" placeholder="Email" required />
+          <textarea placeholder="Type a Message" rows={10} required />
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
