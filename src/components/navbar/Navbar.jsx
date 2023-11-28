58bdd3494c1a9ab158b425d4856b2e7cff2d795e
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-28 21:46:12 +0530

    updated navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,7 +19,8 @@ const Navbar = () => {
         <div className="social">
 
             <a href='https://github.com/HimashaNethmini'><img src="/github.png" alt="" /></a>
-            <a href='https://www.linkedin.com/in/himasha-nethmini-4b32a1231/'><img src="/linkedin.png" alt="" /></a>
+            {/*<a href='https://www.linkedin.com/in/himasha-nethmini-4b32a1231/'><img src="/linkedin.png" alt="" /></a>*/}
+            <a href='https://www.linkedin.com/in/himasha-nethmini-4b32a1231/'><img src="/1.png" alt="" /></a>
             <a href='#'><img src="/youtube.png" alt="" /></a>
             <a href='https://www.hackerrank.com/profile/himashanethmini1'><img src="/hackerrank.png" alt="" /></a>
         </div>
