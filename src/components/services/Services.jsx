537be1991b170f461de77ad419f84e42e91dd5b4
HimashaNethmini <himashanethmini05@gmail.com>
2023-11-30 14:04:35 +0530

    updated contents

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -38,28 +38,39 @@ const Services = () => {
     >
       <motion.div className="textContainer" variants={variants}>
         <p>
-        I am passionate about learning and growing
-          <br />  
-        in a wide range of technologies
+          I am passionate about learning and growing
+          <br />
+          in a wide range of technologies
         </p>
         <hr />
       </motion.div>
 
       <motion.div className="titleContainer" variants={variants}>
-        <div className="title">
-          <p> Hello !I'm Himasha Nethmini, a Final year undergraduate at University of SLIIT, pursuing a BSc (Hons) Spec. in Information Technology. I am a person with a heart full of curiosity and a head buzzing with creativity. Whether it's tinkering with design, coding, problem-solving or diving into the world of IT, I find joy in every aspect of what I do. My journey is painted with a palette of experiences that have sculpted me into someone who's not just about the skills but also about the journey. I believe in continuous learning,collaboration,committment, and always bring a positive vibe to the table. 
-If you're looking for someone who's not just a professional but also a friendly face, let's connect! I'm all ears for new conversations and adventures. Can't wait to chat and explore where creativity takes us! </p>
-          {/*<img src="/people.webp" alt="" />
-          <h1>
-            <motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas
-          </h1>
+        <div className="title1">
+          <p>
+            {" "}
+            Hello <span style={{ color: "orange" }}>!</span> I'm Himasha
+            Nethmini, a Final year undergraduate at University of SLIIT,
+            pursuing a BSc (Hons) Spec. in Information Technology. I am a person
+            with a heart full of curiosity and a head buzzing with creativity.
+            Whether it's tinkering with design, coding, problem-solving or
+            diving into the world of IT, I find joy in every aspect of what I
+            do. My journey is painted with a palette of experiences that have
+            sculpted me into someone who's not just about the skills but also
+            about the journey. I believe in continuous
+            learning,collaboration,committment, and always bring a positive vibe
+            to the table. If you're looking for someone who's not just a
+            professional but also a friendly face, let's connect! I'm all ears
+            for new conversations and adventures. Can't wait to chat and explore
+            where creativity takes us!{" "}
+          </p>
         </div>
-        <div className="title">
-          <h1>
-            <motion.b whileHover={{color:"orange"}}>For Your</motion.b> Business.
-  </h1>*
-  <button>WHAT I DO?</button>*/}
-  </div>
+
+        <div className="type">
+          <button>WHAT I DO?</button>
+        </div>
+          
+        
       </motion.div>
 
       <motion.div className="listContainer" variants={variants}>
@@ -76,20 +87,6 @@ If you're looking for someone who's not just a professional but also a friendly 
           </p>
           <button>Go</button>
         </motion.div>
-        
-        <motion.div
-          className="box"
-          whileHover={{ background: "lightgray", color: "black" }}
-        >
-          <h2>Branding</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
-            animi! Ad nam pariatur assumenda quae mollitia libero repellat
-            explicabo maiores?
-          </p>
-          <button>Go</button>
-        </motion.div>
 
         <motion.div
           className="box"
@@ -117,11 +114,23 @@ If you're looking for someone who's not just a professional but also a friendly 
             explicabo maiores?
           </p>
           <button>Go</button>
+        </motion.div>
 
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
         </motion.div>
       </motion.div>
     </motion.div>
-
   );
 };
 
