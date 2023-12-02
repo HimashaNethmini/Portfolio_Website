8c413cf2165481ccf4a741d8c0875efdc57bd2ec
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-02 19:26:55 +0530

    updated navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,8 +13,7 @@ const Navbar = () => {
         <motion.span initial={{opacity:0, scale:0.5}} 
         animate={{opacity:1, scale:1}}
         transition={{duration:0.5}}>
-          Himasha Nethmini
-        </motion.span>
+       </motion.span>
 
         <div className="social">
 
