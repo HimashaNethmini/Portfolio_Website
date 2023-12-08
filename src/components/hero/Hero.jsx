beaa6cfed337375556f4910ee53bc988371e8015
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-08 21:43:22 +0530

    add js function for auto typing

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,33 +1,44 @@
+import React, { useEffect } from "react";
 import "./hero.scss";
 
 const Hero = () => {
+  useEffect (()=> {
+    //initialize
+    var typed = new Typed(".auto-input",{
+      strings: ["Fullstack Developer", "UI / UX Designer", "Software Engineer"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true
+    });
+    
+    //clean 
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+  //empty dependency
+
   return (
     <div className="hero">
       <div className="wrapper">
         <div className="textContainer">
           <h2>HIMASHA NETHMINI</h2>
-          <h1>
-            Fullstack Developer 
-            {/*<h4 >and</h4>
-             Frontend Developer*/}
-          </h1>
-
+          <h4>I AM </h4>
+            <h1><span class="auto-input"></span></h1>
+          
           <div className="buttons">
-            <button>
-              Download CV
-            </button>
+            <button>Download CV</button>
             <button>Contact Me</button>
           </div>
           <img src="/scroll.png" alt="" />
         </div>
       </div>
-      
+
       <div className="imageContainer">
         <img src="/fed.png" alt="" />
       </div>
     </div>
   );
 };
-
 
 export default Hero;
