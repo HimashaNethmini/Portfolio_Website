4848ff4186b15bf6c3ada4ddb0c4b31ed6e2a05a
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-12 22:15:47 +0530

    added routing

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect } from "react";
 import "./hero.scss";
+import {Link} from "react-scroll";
 
 const Hero = () => {
-  
-  useEffect (()=> {
+
+  useEffect(() => {
     //initialize Typed instance when component mounts
-    var typed = new Typed(".auto-input",{
+    var typed = new Typed(".auto-input", {
       strings: ["Fullstack Developer", "UI / UX Designer", "Software Engineer"],
       typeSpeed: 100,
       backSpeed: 100,
-      loop: true
+      loop: true,
     });
-    
-    return () => { //clean up Typed instance when component unmounts
+
+    return () => {
+      //clean up Typed instance when component unmounts
       typed.destroy();
     };
   }, []); //empty dependency array ensure the effect runs once
@@ -23,16 +25,23 @@ const Hero = () => {
         <div className="textContainer">
           <h2>HIMASHA NETHMINI</h2>
           <h4>I AM </h4>
-            <h1><span class="auto-input"></span></h1>
-          
+          <h1>
+            <span class="auto-input"></span>
+          </h1>
+
           <div className="buttons">
             <button>Download CV</button>
-            <button>Contact Me</button>
+
+            <Link to="contact">
+              <button>Contact Me</button>
+            </Link>
           </div>
-          <img src="/scroll.png" alt="" />
+          
+          <img src="/scroll.png" alt="svg image" />
         </div>
       </div>
-{/* main image */}
+
+      {/* main image */}
       <div className="imageContainer">
         <img src="/fed.png" alt="" />
       </div>
