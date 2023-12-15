31f7665abacc79ddec2208a557f81303c05d72fa
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-15 21:50:11 +0530

    add contents

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -18,13 +18,13 @@ const items = [
   {
     id: 3,
     title: "Care NGO",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "img3.png",
+    desc: "A full-stack webapp developed using MERN stack, organized into four categories: Education, Gender, Health, & Digital Bridge. Admins can perform CRUD operations while the users have a range of functionalities, including the ability to watch educational videos, write and read posts and articles, and make pharmaceutical donations. The platform aims to address diverse areas, offering an inclusive and feature-rich experience for both adminis and users.",
   },
   {
     id: 4,
     title: "Music App",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "img4.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
   {
